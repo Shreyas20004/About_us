b086538fca84dd7f384a17c4840d146eac8c79b9
Shreyas20004 <shreyas2004src@gmail.com>
2025-01-12 18:01:46 +0530

    Statement comming soon

--- src/components/Statement.jsx
+++ src/components/Statement.jsx
@@ -1,9 +1,17 @@
-
+import bg from '../assets/locked.png';
 
 export default function Statement() {
   return (
-    <div>
-      
+    <div className="mt-8 col-span-3 row-span-3 rounded-3xl overflow-hidden hover:cursor-pointer h-96 flex items-center justify-center relative">
+      <div
+        className="absolute inset-0 bg-cover bg-center blur-sm"
+        style={{
+          backgroundImage: `url(${bg})`,
+        }}
+      ></div>
+      <div className="bg-gray-500 bg-opacity-50 text-white text-lg font-semibold p-6 rounded-lg cursor-pointer transition-all ease-in-out duration-300 hover:bg-opacity-75 hover:scale-105 z-10">
+        Coming Soon...
+      </div>
     </div>
-  )
+  );
 }
